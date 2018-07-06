4b209c8637204d226ada4cf20affc214ec4053cf
John Bonney <john.fbonney@gmail.com>
2018-07-06 16:08:29 -0500

    Added documentation and error checks for stringrow function

--- quiz-results/textables/R/stringrow.R
+++ quiz-results/textables/R/stringrow.R
@@ -1,9 +1,22 @@
-
+#' Function to create rows of text in LaTeX table.
+#'
+#' @description This function creates rows of text (in
+#'  characters) in a LaTeX table.
+#'
+#' @param title The title of the row (character).
+#' @param array The array that contains the information
+#'  that will be in the row.
+#' @param spacer The LaTeX code for spacing between this
+#'  row and the next row (character).
 
 stringrow <- function(title, array, spacer = "\\\\"){
-
+  if (!is.character(title)){
+    stop(sprintf("Input `title` must be character but is %s.\n", class(title)))
+  }
+  if (!is.character(spacer)){
+    stop(sprintf("Input `spacer` must be character but is %s.\n", class(spacer)))
+  }
   y <- paste(paste0(" & ", array), collapse = "")
   return(paste0(title, y, spacer))
-
 }
 
