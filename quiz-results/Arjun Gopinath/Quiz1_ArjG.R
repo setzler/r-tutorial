3c24265b25b2c41bbe9f407cd1d5622bd1b8cf22
Bradley Setzler <setzler@users.noreply.github.com>
2018-08-02 13:22:13 -0500

    Fixed conflict resolve error

--- quiz-results/Arjun Gopinath/Quiz1_ArjG.R
+++ quiz-results/Arjun Gopinath/Quiz1_ArjG.R
@@ -6,9 +6,9 @@ library(data.table)
 
 # Defining the function to convert temperature between Celsius and Fahrenheit.
 conv_temp <- function(temperature, FtoC = TRUE, options) {
-
+  
   # Check if temperature is a numeric vector.
-  if (!(is.numeric(temperature))) {
+    if (!(is.numeric(temperature))) {
     stop("Temperature must be a numeric vector.")
   }
   
@@ -16,19 +16,40 @@ conv_temp <- function(temperature, FtoC = TRUE, options) {
   if ((!(is.logical(FtoC))) || (length(FtoC) != 1)) {
     stop("FtoC must be a single Boolean variable.")
   }  
-  
+   
   # Check if options is a list.
   if (!(class(options) == "list")) {
     stop("Options must be a list.")
   }
   
+  # Check if temperature is given in F. If true, convert to C
+  if (FtoC == TRUE) {
+    fahrenheit = temperature
+    celsius = (temperature - 32) * 5 / 9
+  } 
+  
+  # If false, convert temperature to F
+  else {
+    celsius = temperature
+    fahrenheit = (temperature * 9 / 5) + 32
+  }
+  
+  # Check if options contains option1. 
+  if (!("option1" %in% options)) {
+    stop("options$option1 must exist.") 
+  }
+  
+  # Tabulate results into a data table.
+  results_table <- data.table(Celsius = celsius, Fahrenheit = fahrenheit)
+  
+  # Return as output.
+  return(results_table)
+}
 
 # Different examples to prove that the code works.
-
 conv_temp(c(25, 32, 100, 40 ), T, list("option1"))
 conv_temp(c(0, -27, 200, 40 ), F, list("option1"))
 conv_temp(temperature = c(25, 32, 100, 40 ), options = list("option2", "option1"))
 conv_temp(c(23, 45), F, list("option2", "option3"))
 conv_temp(c(23, 45), F, c("option1", "option2"))
-
-
+          
