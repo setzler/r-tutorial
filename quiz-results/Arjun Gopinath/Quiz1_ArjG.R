7f34b99c2e364afc0234baa75a8c65b28180366c
Arjun Gopinath <arj.arjun.jun@gmail.com>
2018-08-02 14:19:51 -0500

    Quiz 1 updates.

--- quiz-results/Arjun Gopinath/Quiz1_ArjG.R
+++ quiz-results/Arjun Gopinath/Quiz1_ArjG.R
@@ -48,14 +48,8 @@ conv_temp <- function(temperature, FtoC = TRUE, options) {
 
 # Different examples to prove that the code works.
 
-=======
-
 conv_temp(c(25, 32, 100, 40 ), T, list("option1"))
 conv_temp(c(0, -27, 200, 40 ), F, list("option1"))
 conv_temp(temperature = c(25, 32, 100, 40 ), options = list("option2", "option1"))
 conv_temp(c(23, 45), F, list("option2", "option3"))
 conv_temp(c(23, 45), F, c("option1", "option2"))
-
-
-=======
-          
