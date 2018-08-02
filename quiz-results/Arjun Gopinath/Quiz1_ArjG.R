e8aeaafff3956b403ab33e22af6e2b7d2cfad975
Arjun Gopinath <arj.arjun.jun@gmail.com>
2018-08-02 13:14:06 -0500

    Updating Quiz 1 (#8)
    * Add files via upload
    
    Results of Quiz 2.
    
    * Uploading my results of Quizzes 1 and 2.
    
    * Updated Quiz 1.

--- quiz-results/Arjun Gopinath/Quiz1_ArjG.R
+++ quiz-results/Arjun Gopinath/Quiz1_ArjG.R
@@ -1,3 +1,4 @@
+
 # Arjun Gopinath - Aug 1 2018
 # R Tutorial Quiz 1
 
@@ -5,35 +6,29 @@ library(data.table)
 
 # Defining the function to convert temperature between Celsius and Fahrenheit.
 conv_temp <- function(temperature, FtoC = TRUE, options) {
-  
-  # Check if temperature is given in F. If true, convert to C
-  if (FtoC == TRUE) {
-    fahrenheit = temperature
-    celsius = (temperature - 32) * 5 / 9
-  } 
-  
-  # If false, convert temperature to F
-  else {
-    celsius = temperature
-    fahrenheit = (temperature * 9 / 5) + 32
+
+  # Check if temperature is a numeric vector.
+  if (!(is.numeric(temperature))) {
+    stop("Temperature must be a numeric vector.")
   }
   
-  # Check if options contains option1. 
-  if (!("option1" %in% options)) {
-    stop("options$option1 must exist.") 
+  # Check if FtoC is a boolean.
+  if ((!(is.logical(FtoC))) || (length(FtoC) != 1)) {
+    stop("FtoC must be a single Boolean variable.")
+  }  
+  
+  # Check if options is a list.
+  if (!(class(options) == "list")) {
+    stop("Options must be a list.")
   }
   
-  # Tabulate results into a data table.
-  results_table <- data.table(Celsius = celsius, Fahrenheit = fahrenheit)
-  
-  # Return as output.
-  return(results_table)
-}
 
 # Different examples to prove that the code works.
-conv_temp(c(25, 32, 100, 40 ), T, "option1")
-conv_temp(c(0, -27, 200, 40 ), F, "option1")
-conv_temp(temperature = c(25, 32, 100, 40 ), options = c("option2", "option1"))
-conv_temp(c(23, 45), F, c("option2", "option3"))
-conv_temp(c(23, 45), F, "option1")
-          
+
+conv_temp(c(25, 32, 100, 40 ), T, list("option1"))
+conv_temp(c(0, -27, 200, 40 ), F, list("option1"))
+conv_temp(temperature = c(25, 32, 100, 40 ), options = list("option2", "option1"))
+conv_temp(c(23, 45), F, list("option2", "option3"))
+conv_temp(c(23, 45), F, c("option1", "option2"))
+
+
